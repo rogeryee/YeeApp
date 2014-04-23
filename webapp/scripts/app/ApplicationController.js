582bcc21273aa973f7c84cc2ea65efeb7e35e6ff
roger.yee <roger.yee@hotmail.com>
2014-04-23 17:21:56 +0800

    update with RequireJS and remove the denpendency of Dojo

--- webapp/scripts/app/ApplicationController.js
+++ webapp/scripts/app/ApplicationController.js
@@ -1,9 +1,24 @@
+/// <reference path="lib/angular/angular.d.ts"/>
 "use strict";
 define(["require", "exports"], function(require, exports) {
     var ApplicationController = (function () {
-        function ApplicationController() {
+        function ApplicationController(application) {
+            this.application = application;
             console.log("Create ApplicationController");
+
+            application.config(function ($stateProvider, $urlRouterProvider) {
+                // For any unmatched url, send to /route1
+                $urlRouterProvider.otherwise("/route1");
+
+                $stateProvider.state('route1', {
+                    url: "/route1",
+                    template: "login.html"
+                });
+            });
         }
+        ApplicationController.prototype.getApplication = function () {
+            return this.application;
+        };
         return ApplicationController;
     })();
     exports.ApplicationController = ApplicationController;
--- webapp/scripts/app/ApplicationController.ts
+++ webapp/scripts/app/ApplicationController.ts
@@ -1,10 +1,28 @@
-"use strict";
+/// <reference path="lib/angular/angular.d.ts"/>
 
+"use strict";
 
 export class ApplicationController
 {
-	constructor()
+	constructor(private application:any)
 	{
 		console.log("Create ApplicationController");
+		
+		application.config(($stateProvider, $urlRouterProvider) => {  
+      
+		  // For any unmatched url, send to /route1
+	      $urlRouterProvider.otherwise("/route1");
+	      
+	      $stateProvider
+	        .state('route1', {
+	            url: "/route1",
+	            template: "login.html"
+	        });
+	    });
+	}
+	
+	public getApplication():any
+	{
+		return this.application;
 	}
 }
